48b57b35a6523f3547fcb49d1c2aa80a6b2606de
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-05-13 15:48:31 +0800

    Add files via upload

---
+++ algorithm exp/BinaryTree.cpp
@@ -0,0 +1,166 @@
+#include"exp.h"
+struct treenode
+{
+    char val;
+    treenode* left;
+    treenode* right;
+};
+class tree
+{
+public:
+    tree()
+    {
+        root = new treenode;
+        root = NULL;
+        //root->left = nullptr;
+        //root->right = nullptr;
+    }
+    tree(string s)
+    {
+        int pos = -1;
+        root = new treenode;
+        create(s, pos,root);
+    }
+    void create (string s,int &pos,treenode *&root)
+    {
+        ++pos;
+        //treenode*ptr=root;
+        root = new treenode;
+        if (pos == s.size())
+        {
+            //cout << "Create complete" << endl;
+            root=NULL;
+            return;
+        }
+        else
+        {
+            if (pos>s.size()-1||s[pos] == '*')
+            {
+                root = NULL;
+                return;
+            }
+            else
+            {
+                root->val = s[pos];
+                create(s, pos,root->left);
+                create(s, pos,root->right);
+            }
+        }
+
+
+    }
+    ~tree()
+    {
+        delete root;
+        cout<<"The binary tree you have just built has been ruined!"<<endl;
+    }
+    int maxdepth(treenode* ptr)//返回最大深度
+    {
+        if (!ptr)
+            return 0;
+        int left = maxdepth(ptr->left);
+        int right = maxdepth(ptr->right);
+        return max(left, right) + 1;
+    }
+    void preordertraversal(treenode* ptr)//前序遍历
+    {
+        if (!ptr)
+        {
+            cout << "Your tree is empty!" << endl;
+            return;
+        }
+        stack<treenode*> temp;
+        temp.push(ptr);
+        vector<char> res;
+        treenode* p = ptr;
+        while (!temp.empty())
+        {
+            p = temp.top();
+            temp.pop();
+            res.emplace_back(p->val);
+            if (p->right)temp.push(p->right);
+            if (p->left)temp.push(p->left);
+        }
+        print(res);
+    }
+    void inordertraversal(treenode* ptr)//中序遍历
+    {
+        if (!ptr)
+        {
+            cout << "Your tree is empty!" << endl;
+            return;
+        }
+        stack<treenode*> temp;
+        temp.push(ptr);
+        vector<char> res;
+        treenode* p = ptr;
+        while (!temp.empty())
+        {
+            while ((p)&&(p->left))
+            {
+                p = p->left;
+                temp.push(p);
+            }
+            p = temp.top();
+            temp.pop();
+            res.emplace_back(p->val);
+            p = p->right;
+            if(p)
+                temp.push(p);
+        }
+        print(res);
+    }
+    void postordertraversal(treenode* ptr)//后序遍历
+    {
+        if (!ptr)
+        {
+            cout << "Your tree is empty!" << endl;
+            return;
+        }
+        stack<treenode*> temp1;
+        stack<treenode*> temp2;
+        temp1.push(ptr);
+        vector<char> res;
+        treenode* p = ptr;
+        while (!temp1.empty())
+        {
+            p = temp1.top();
+            temp1.pop();
+            temp2.push(p);
+            if (p->left)temp1.push(p->left);
+            if (p->right)temp1.push(p->right);
+        }
+        while(!temp2.empty())
+        {
+            res.emplace_back(temp2.top()->val);
+            temp2.pop();
+        }
+        print(res);
+    }
+    void print(vector<char> a)
+    {
+        for (vector<char>::iterator it = a.begin(); it != a.end(); it++)
+        {
+            cout << *it << " ";
+        }
+        cout<<endl;
+    }
+    treenode* root;
+};
+int main()
+{
+    tree test("12*3");
+    //cout << test.root->val<<endl;
+    cout<<"The result of preordertraversal is:"<<endl;
+    test.preordertraversal(test.root);//前序遍历
+    cout<<"The result of inordertraversal is:"<<endl;
+    test.inordertraversal(test.root);//中序遍历
+    cout<<"The result of postordertraversal is:"<<endl;
+    test.postordertraversal(test.root);//后序遍历
+    cout<<"The binary tree's maximum depth is "<<test.maxdepth(test.root)<<endl;
+    //int a[1] = {  };
+}
+//
+// Created by lee on 2021/5/12.
+//
+
---
+++ algorithm exp/cmake-build-debug/CMakeFiles/algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm.dir/BinaryTree.cpp.obj"
+  "algorithm.exe"
+  "algorithm.exe.manifest"
+  "algorithm.lib"
+  "algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
