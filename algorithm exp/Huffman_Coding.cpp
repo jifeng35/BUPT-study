ae8830c0bf4c0c8a0e0dd1af2c0767e14e385674
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-05-14 16:25:09 +0800

    Add files via upload

--- algorithm exp/Huffman_Coding.cpp
+++ algorithm exp/Huffman_Coding.cpp
@@ -13,6 +13,65 @@ public:
         HuffMan(string s)
         {
             string t=get_time(s);
+           while(s.size()==2&&s[0]=='*')
+            {
+                if (t[t.size() - 2] != '*' && t[t.size() - 4] != '*') {
+                    Creat_NewTree(t);
+                } else {
+                    Insert_Node(t);
+                }
+            }
+        }
+        void Update(string &s)
+        {
+
+        }
+        void Creat_NewTree(string &s)
+        {//所含有字符的总数,s这个字符串应为N*("字符"+"权重")构成
+            Huffman_Node*ptr=new Huffman_Node;
+            ptr->left=new Huffman_Node;
+            ptr->right=new Huffman_Node;
+            ptr->left->right=ptr->left->left=ptr->right->left=ptr->right->right=NULL;
+            ptr->left->data=s[s.size()-2];
+            ptr->left->Heavy_Weight=int(s[s.size()-1]);
+            s.pop_back();s.pop_back();
+            ptr->right->data=s[s.size()-2];
+            ptr->right->Heavy_Weight=int(s[s.size()-1]);
+            ptr->Heavy_Weight= ptr->left->Heavy_Weight+ ptr->right->Heavy_Weight;
+            s.pop_back();s.pop_back();
+            s.push_back('*');
+            s.push_back(ptr->Heavy_Weight+48);
+            root=ptr;
+        }
+        void Insert_Node(string &s)
+        {
+            Huffman_Node*ptr=new Huffman_Node;
+            if(s[s.size()-2]=='*') {
+                ptr->right = new Huffman_Node;
+                ptr->right->data = s[s.size() - 4];
+                ptr->right->Heavy_Weight=int(s[s.size()-3]);
+                ptr->right->left=ptr->right->right=NULL;
+                s.pop_back();s.pop_back();
+                ptr->left= root;
+                ptr->Heavy_Weight= ptr->left->Heavy_Weight+ ptr->right->Heavy_Weight;
+                s.pop_back();s.pop_back();
+                s.push_back('*');
+                s.push_back(ptr->Heavy_Weight+48);
+            }
+                else if(s[s.size()-4]=='*')
+            {
+                ptr->left = new Huffman_Node;
+                ptr->left->data = s[s.size() - 2];
+                ptr->left->Heavy_Weight=int(s[s.size()-1]);
+                ptr->left->right=ptr->left->left=NULL;
+                s.pop_back();s.pop_back();
+                ptr->right= root;
+                ptr->Heavy_Weight= ptr->left->Heavy_Weight+ ptr->right->Heavy_Weight;
+                s.pop_back();s.pop_back();
+                s.push_back('*');
+                s.push_back(ptr->Heavy_Weight+48);
+            }
+            root=ptr;
         }
         string get_time(string s)
         {
