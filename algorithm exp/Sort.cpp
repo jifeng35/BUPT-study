fd363045e4cd0d7b3eea3399dbff6f8a8087c121
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-06-03 00:11:41 +0800

    Add files via upload

--- algorithm exp/Sort.cpp
+++ algorithm exp/Sort.cpp
@@ -2,14 +2,16 @@
 // Created by lee on 2021/6/2.
 //
 #include"Sort.h"
+const int MAX=10000;
 int main(){
     my_sort test;
-    vector<int> a(10000,0);
+    vector<int> a(MAX,0);
     srand(time(NULL));
-    for(int i=0;i<10000;i++)
+    for(int i=0;i<MAX;i++)
     {
         a[i]=rand();
     }
     test.Bubble(a);
+    test.Quick(a);
     test.print_time();
 }
--- algorithm exp/Sort.h
+++ algorithm exp/Sort.h
@@ -12,7 +12,7 @@ public:
     void Bubble(vector<int> a);
     void bubble(vector<int> a);
     void Quick(vector<int> a);
-    void quick(vector<int> a,int low,int high);
+    void quick(vector<int> &a,int low,int high);
     void print(vector<int> a);
     void print_time();
 private:
@@ -48,20 +48,31 @@ void my_sort::Bubble(vector<int> a){
 }
 void my_sort::print_time(){
     cout<<"Bubble_Sort Time is "<<Bubble_Time<<"s"<<endl;
+    cout<<"Quick_Sort Time is "<<Quick_Time<<"s"<<endl;
 }
-void my_sort::quick(vector<int> a,int low,int high){
-    if(low==high){
+void my_sort::quick(vector<int> &a,int low,int high){
+    if(low>=high){
         return;
     }
     int left=low;
     int right=high;
     int key=a[left];
-    while(right<left){
-            while()
+    while(right!=left){
+            while(left<right&&a[right]>=key)
+                --right;
+            while(left<right&&a[left]<=key)
+                ++left;
+            a[right]=a[left];
     }
+    a[left]=key;
     quick(a,low,left-1);
     quick(a,left+1,high);
 }
 void my_sort::Quick(vector<int> a){
-
+    vector<int> b=a;
+    clock_t s_time=clock();
+    quick(b,0,b.size()-1);
+    clock_t e_time=clock();
+    Quick_Time=(double)(e_time-s_time)/CLOCKS_PER_SEC;
+    print(b);
 }
