fcd17cdb39f11f8fdf0fd545c26820b840343999
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-06-02 18:16:59 +0800

    Add files via upload

--- algorithm exp/Sort.cpp
+++ algorithm exp/Sort.cpp
@@ -1,4 +1,12 @@
 //
 // Created by lee on 2021/6/2.
 //
+#include"Sort.h"
+int main(){
+    my_sort test;
+    int a[]={
+            2,3,5,2,4,14,6565,786,45,
+    };
+    test.Bubble(a,sizeof(a)/sizeof(a[0]));
 
+}
--- algorithm exp/Sort.h
+++ algorithm exp/Sort.h
@@ -6,3 +6,28 @@
 #define UNTITLED1_SORT_H
 
 #endif //UNTITLED1_SORT_H
+#include "exp.h"
+class my_sort{
+public:
+    void Bubble(int a[],int len){
+        for(int i=0;i<len-1;++i){
+            bool no_swap=true;
+            for(int j=0;j<len-i-1;++j){
+                if(a[j]>a[j+1]) {
+                    a[j] = a[j] + a[j + 1];
+                    a[j + 1] = a[j] - a[j + 1];
+                    a[j] = a[j] - a[j + 1];
+                    no_swap=false;
+                }
+            }
+            if(no_swap)break;
+        }
+        print(a,len);
+    }
+    void print(int a[],int len){
+        for(int i=0;i<len;++i){
+            cout<<a[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
