f5d6b6f36f4ac90de2fe2037180570bedd65f27b
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-06-06 10:56:22 +0800

    Add files via upload

--- algorithm exp/Binary_Search_Tree.h
+++ algorithm exp/Binary_Search_Tree.h
@@ -9,9 +9,40 @@
 #include"exp.h"
 struct BS_Node{
     int val;
+    BS_Node *parent;
     BS_Node *left;
     BS_Node *right;
 };
 class BS_Tree{
-
+public:
+    BS_Tree();
+    void createBSTree();
+    void search(int val);
+    void erase(int val);
+    void insert(int val);
+    BS_Node * find_pos(int val);
+    ~BS_Tree();
+private:
+    BS_Node*root;
 };
+BS_Tree::BS_Tree(){
+
+}
+void BS_Tree::createBSTree(){
+
+}
+void BS_Tree::erase(int val) {
+
+}
+void BS_Tree::search(int val){
+
+}
+void BS_Tree::insert(int val){
+
+}
+BS_Node *BS_Tree::find_pos(int val){
+
+}
+BS_Tree::~BS_Tree(){
+
+}
