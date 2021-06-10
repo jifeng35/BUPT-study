818f2dd0ef34c4840856ce427f120fc18e24f409
jifeng35 <84109179+jifeng35@users.noreply.github.com>
2021-06-10 10:28:44 +0800

    Add files via upload

--- algorithm exp/BB_Tree.cpp
+++ algorithm exp/BB_Tree.cpp
@@ -6,8 +6,8 @@
 int main(){
     vector<int> a={1,2,3,4,5,6,7};
     BB_Tree test(a);
-    test.Level_Traversal();//改旋转函数
+    test.Level_Traversal();
     test.search(4);
     test.erase(test,4,'a');
-    test.Level_Traversal();//改旋转函数
+    test.Level_Traversal();
 }
